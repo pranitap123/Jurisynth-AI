fae3786e84347e9d5374dc8bf38b22b12c2a7ef9
Pranita <pranitapanchal339@gmail.com>
2025-12-05 16:20:53 +0530

    Added CI/CD Pipline

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './styles.css'; 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
-// Import your pages
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 import Dashboard from './components/dashboard/Dashboard';
@@ -12,31 +11,24 @@ import GlobalSearchPage from './pages/GlobalSearchPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        {/* Your Homepage */}
         <Route path="/" element={<HomePage />} />
         
-        {/* Your Login/Register Page */}
         <Route path="/auth" element={<AuthPage />} />
         
-        {/* Your Main Dashboard Page */}
         <Route path="/dashboard" element={<Dashboard />} />
         
-        {/* The Page for a Single Case */}
         <Route path="/case/:caseId" element={<CaseViewPage />} />
         
-        {/* The Page for Listing All Cases */}
         <Route path="/all-cases" element={<AllCasesPage />} /> 
 
-        {/* The Page for Global Search */}
         <Route path="/search" element={<GlobalSearchPage />} /> 
 
-        {/* You can add more routes here later */}
         <Route path="/settings" element={<div>Settings Page</div>} />
         <Route path="/forgot-password" element={<div>Forgot Password Page</div>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
